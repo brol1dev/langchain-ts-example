daa87c485ae950c579bd74e4ee7621fc0d9e6d90
Eric Vargas <eric.vargas.mx@gmail.com>
2023-04-29 22:52:27 -0500

    Livechart.me WebBrowser example

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import * as dotenv from "dotenv";
-import { OpenAI } from "langchain";
+import { OpenAI } from "langchain/llms/openai";
+import { WebBrowser } from "langchain/tools/webbrowser";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
 dotenv.config();
 
-const model = new OpenAI({
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: process.env.OPENAI_API_KEY,
-});
+// const model = new OpenAI({
+//   modelName: "gpt-3.5-turbo",
+//   openAIApiKey: process.env.OPENAI_API_KEY,
+// });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+// const res = await model.call(
+//   "What's a good idea for an application to build with GPT-3?"
+// );
 
-console.log(res);
+// console.log(res);
+
+export async function run() {
+  const model = new ChatOpenAI({ temperature: 0 });
+  const embeddings = new OpenAIEmbeddings();
+
+  const browser = new WebBrowser({ model, embeddings });
+
+  const result = await browser.call(
+    `"https://www.livechart.me/", "Cuando saldra el nuevo episodio de Kimetsu no yaiba?"`
+  );
+
+  console.log(result);
+}
+
+await run();
